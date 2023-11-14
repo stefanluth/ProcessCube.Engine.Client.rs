388ca7d957bbd1f69f8181193cf24a1e149796a6
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-11-14 10:57:17 +0100

    add integration tests

---
+++ tests/application_info_client.rs
@@ -0,0 +1,39 @@
+use engine_client::clients::client_factory::ClientFactory;
+
+const DUMMY_TOKEN: &str = "Bearer ZHVtbXlfdG9rZW4=";
+const ENGINE_URL: &str = "http://localhost:10560";
+
+#[tokio::test]
+async fn get_application_info() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let client = client_factory.create_application_info_client();
+    let result = client.get_application_info().await;
+
+    if let Err(e) = result {
+        panic!("Error getting application info: {:#?}", e);
+    }
+
+    let application_info = result.unwrap();
+
+    assert_ne!(application_info.id, None);
+    assert_eq!(application_info.name, "5Minds Engine");
+    assert_eq!(application_info.package_name, "@5minds/processcube_engine");
+    assert_eq!(application_info.authority_url, "http://localhost:11560/");
+    assert_ne!(application_info.version, "");
+    assert!(application_info.allow_anonymous_root_access);
+}
+
+#[tokio::test]
+async fn get_authority_info() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let client = client_factory.create_application_info_client();
+    let result = client.get_authority_info().await;
+
+    if let Err(e) = result {
+        panic!("Error getting authority info: {:#?}", e);
+    }
+
+    let authority_info = result.unwrap();
+
+    assert_eq!(authority_info, "http://localhost:11560/");
+}
---
+++ tests/client_factory.rs
@@ -0,0 +1,28 @@
+use engine_client::clients::client_factory::ClientFactory;
+
+const DUMMY_TOKEN: &str = "Bearer ZHVtbXlfdG9rZW4=";
+const ENGINE_URL: &str = "http://localhost:10560";
+
+#[test]
+fn create_application_info_client() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let _client = client_factory.create_application_info_client();
+}
+
+#[test]
+fn create_event_client() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let _client = client_factory.create_event_client();
+}
+
+#[test]
+fn create_process_definition_client() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let _client = client_factory.create_process_definition_client();
+}
+
+#[test]
+fn create_process_model_client() {
+    let client_factory = ClientFactory::new(ENGINE_URL, DUMMY_TOKEN);
+    let _client = client_factory.create_process_model_client();
+}
